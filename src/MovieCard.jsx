e1fcd08356dd9df8b0e11f0bd60f7f4010db00fa
Risik34 <143924962+Risik34@users.noreply.github.com>
2024-02-23 06:28:16 +0000

    Modified movie card

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,7 +4,6 @@ export default function MovieCard({ movie }) {
   // Destructure the movie object to get the relevant properties
   const { Title, Year, Director, Poster, imdbID } = movie;
 
-
   return (
 
     <div className=" my-6 w-auto  sm:w-1/2 md:w-1/3 lg:w-1/4  bg-secondary rounded-md shadow-md" >
